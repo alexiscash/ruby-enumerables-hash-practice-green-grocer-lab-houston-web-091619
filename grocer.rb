82dd0d95b922266731dc3277d95097542ecc24d5
Alexis Rangel <alexiscash97@gmail.com>
2019-09-06 03:39:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -67,5 +67,5 @@ grocery_cart = [
 ]
 
 new_grocery_cart = consolidate_cart(grocery_cart)
-#p apply_coupons(new_grocery_cart, coupons)
-p checkout(grocery_cart, coupons)
+p apply_coupons(new_grocery_cart, coupons)
+#p checkout(grocery_cart, coupons)
