95ea3a52492a385a9f6fd80671903e041fe29d46
Alexis Rangel <alexiscash97@gmail.com>
2019-09-06 02:57:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,7 +16,7 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     if cart.include?(coupon[:item])
-      puts cart[coupon[:item]]
+      puts cart[coupon[:item]][:price]
     end
   end 
   cart
