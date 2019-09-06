18774580ae97bd6a79dafb5e9fab39453bb84c27
Alexis Rangel <alexiscash97@gmail.com>
2019-09-06 02:40:53 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -36,8 +36,8 @@ end
 def checkout(cart, coupons)
   cart = consolidate_cart(cart)
   cart = apply_clearance(cart)
-  cart.each do |memo, n|
-    puts memo, n
+  cart.each do |item, attributes|
+    puts 
   end 
 end
 
